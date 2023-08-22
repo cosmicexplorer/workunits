8578d781b320f826bf858388b3aed26088d4c551
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2023-08-22 13:47:08 -0400

    lmao make tests pass

--- lib/src/streams.rs
+++ lib/src/streams.rs
@@ -11,9 +11,9 @@
 //! ???
 
 
-pub mod referencing {
+pub(crate) mod referencing {
   macro_rules! uuid_struct {
-    ($name:id) => {
+    ($name:ident) => {
       /// <id: {0}>
       #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, ::displaydoc::Display)]
       pub struct $name(::uuid::Uuid);
@@ -57,7 +57,7 @@ pub mod indexing {
     type Key = Name;
 
     /* TODO: make this match more complex expressions, etc. */
-    pub fn matches(&self, name: &Name) -> bool { self.0 == name.0 }
+    fn matches(&self, name: &Name) -> bool { self.0 == name.0 }
   }
 }
 
@@ -73,10 +73,13 @@ pub mod mux {
   use parking_lot::RwLock;
 
   use std::{
-    cmp,
     collections::VecDeque,
+    hash::Hash,
     io, mem,
-    sync::{mpsc, Arc},
+    sync::{
+      atomic::{AtomicBool, Ordering},
+      mpsc, Arc,
+    },
   };
 
   pub trait StreamID {
@@ -86,24 +89,27 @@ pub mod mux {
 
   pub type Payload = Bytes;
 
-  pub trait ListenStream: StreamID+io::Read {}
+  pub trait ListenStream: StreamID+io::Read {
+    fn signal_end(&self);
+  }
 
   pub trait WriteStream: StreamID+io::Write {}
 
   pub trait Streamer {
     type Listener: ListenStream;
     type Query: Matches<Key=Self::Name>;
-    fn open_listener(&mut self, query: Self::Query) -> Self::Listener;
+    fn open_listener(&self, query: Self::Query) -> Self::Listener;
 
     type Writer: WriteStream;
     type Name;
-    fn open_writer(&mut self, name: Self::Name) -> Self::Writer;
+    fn open_writer(&self, name: Self::Name) -> Self::Writer;
   }
 
   pub struct ListenMux {
     id: ReadStreamId,
     recv: mpsc::Receiver<Payload>,
     remainder: VecDeque<u8>,
+    end_signal: AtomicBool,
     index: Arc<RwLock<StreamMux>>,
   }
 
@@ -117,6 +123,7 @@ pub mod mux {
         id,
         recv,
         remainder: VecDeque::new(),
+        end_signal: AtomicBool::new(false),
         index,
       }
     }
@@ -125,12 +132,13 @@ pub mod mux {
   impl StreamID for ListenMux {
     type ID = ReadStreamId;
 
-    fn id(&self) -> Self::ID { *self.id }
+    fn id(&self) -> Self::ID { self.id }
   }
 
-  /// It's recommended to wrap this impl with a buffering reader.
   impl io::Read for ListenMux {
-    /// A read() which may block and may not fill the whole buffer.
+    /// A read() which may block and may not fill the whole buffer, but which
+    /// will only block the minimum possible time necessary in order to
+    /// return a non-zero value.
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
       if buf.is_empty() {
         return Ok(0);
@@ -145,30 +153,46 @@ pub mod mux {
       }
       assert!(self.remainder.is_empty());
 
-      /* Otherwise, we wait to pull in anything (we *don't* wait to fill the whole
-       * buffer). */
-      match self.recv.recv() {
-        /* This occurs when all senders disconnect, which should never happen because we always
-         * cache an extra SyncSender in ListenerContext. */
-        Err(mpsc::RecvError) => unreachable!(),
-        Ok(payload) => {
-          assert!(!payload.is_empty());
-          self.remainder.extend(payload.into_iter());
-          /* We read from the beginning of buf because we early returned if we had
-           * already written any bytes to buf earlier. */
-          let written = self.remainder.read(buf)?;
-          /* We checked that buf and payload are non-empty, so this should always be
-           * true. Any nonzero value is acceptable for a read()
-           * implementation without implicitly signaling EOF. */
-          assert!(written > 0);
-          /* self.remainder may or may not be empty at this point (either is allowed). */
-          Ok(written)
-        },
-      }
+      /* Check to see if we've been told there's no use waiting anymore. */
+      let payload = if self.end_signal.load(Ordering::Acquire) {
+        match self.recv.try_recv() {
+          /* This occurs when all senders disconnect, which should never happen because we always
+           * cache an extra SyncSender in ListenerContext. */
+          Err(mpsc::TryRecvError::Disconnected) => unreachable!(),
+          /* If we've been signalled that it's all empty, then we can signal EOF. */
+          Err(mpsc::TryRecvError::Empty) => {
+            return Ok(0);
+          },
+          Ok(payload) => payload,
+        }
+      } else {
+        /* Otherwise, we wait to pull in anything (we *don't* wait to fill the whole
+         * buffer). */
+        match self.recv.recv() {
+          /* This occurs when all senders disconnect, which should never happen because we always
+           * cache an extra SyncSender in ListenerContext. */
+          Err(mpsc::RecvError) => unreachable!(),
+          Ok(payload) => payload,
+        }
+      };
+      assert!(!payload.is_empty());
+      /* TODO: consider ringbuf crate! https://docs.rs/ringbuf/latest/ringbuf/ */
+      self.remainder.extend(payload);
+      /* We read from the beginning of buf because we early returned if we had
+       * already written any bytes to buf earlier. */
+      let written = self.remainder.read(buf)?;
+      /* We checked that buf and payload are non-empty, so this should always be
+       * true. Any nonzero value is acceptable for a read()
+       * implementation without implicitly signaling EOF. */
+      assert!(written > 0);
+      /* self.remainder may or may not be empty at this point (either is allowed). */
+      Ok(written)
     }
   }
 
-  impl ListenStream for ListenMux {}
+  impl ListenStream for ListenMux {
+    fn signal_end(&self) { self.end_signal.store(true, Ordering::Release); }
+  }
 
   impl Drop for ListenMux {
     fn drop(&mut self) { self.index.write().remove_listener(self.id()); }
@@ -193,7 +217,7 @@ pub mod mux {
   impl StreamID for WriteMux {
     type ID = WriteStreamId;
 
-    fn id(&self) -> Self::ID { *self.id }
+    fn id(&self) -> Self::ID { self.id }
   }
 
   /// It's recommended to wrap this in a buffered writer as well to work around
@@ -211,6 +235,7 @@ pub mod mux {
       let buf = Bytes::copy_from_slice(buf);
       /* TODO: rayon/etc */
       for sender in self.tees.read().values() {
+        #[allow(clippy::single_match_else)]
         match sender.send(buf.clone()) {
           Ok(()) => (),
           /* This only occurs if the receiver has hung up in between when we read from self.tees
@@ -233,7 +258,7 @@ pub mod mux {
       /* NB: This needs to run before .remove_writer() to ensure that the StreamMux
        * handle has the only Arc<RwLock<IndexMap<...>>> handle so it can call
        * Arc::into_inner(). */
-      mem::drop(tees);
+      let _ = mem::replace(tees, Arc::new(RwLock::new(IndexMap::new())));
       index.write().remove_writer(*id);
     }
   }
@@ -256,7 +281,7 @@ pub mod mux {
     pub live_outputs: Arc<RwLock<IndexMap<ReadStreamId, mpsc::SyncSender<Payload>>>>,
   }
 
-  struct StreamMux {
+  pub(crate) struct StreamMux {
     /* This is the "reverse index", only used for bookkeeping when adding and removing r/w
      * streams. */
     listeners: IndexMap<ReadStreamId, ListenerContext>,
@@ -272,7 +297,10 @@ pub mod mux {
       }
     }
 
-    fn find_matching_writers(&self, query: &Query) -> impl Iterator<Item=WriteStreamId> {
+    fn find_matching_writers<'a>(
+      &'a self,
+      query: &'a Query,
+    ) -> impl Iterator<Item=WriteStreamId>+'a {
       /* TODO: rayon? */
       /* TODO: use some caching to avoid a linear scan each time? */
       self
@@ -296,29 +324,26 @@ pub mod mux {
       /* (1) Add all the write targets teeing to this read stream into the reverse
        * index. */
       let write_targets: IndexSet<WriteStreamId> = self.find_matching_writers(&query).collect();
-      if let Some(_) = self.listeners.insert(new_read_id, ListenerContext {
-        query,
-        sender: sender.clone(),
-        live_inputs: Arc::new(RwLock::new(write_targets.clone())),
-      }) {
-        unreachable!(
-          "assumed listener stream id {:?} was not already registered",
-          new_read_id
-        );
-      }
+      assert!(self
+        .listeners
+        .insert(new_read_id, ListenerContext {
+          query,
+          sender: sender.clone(),
+          live_inputs: Arc::new(RwLock::new(write_targets.clone())),
+        })
+        .is_none());
       /* (2) Add this read stream to all the write streams' tee outputs. */
       /* TODO: rayon? */
       for target in write_targets.into_iter() {
         let live_outputs: Arc<RwLock<IndexMap<ReadStreamId, mpsc::SyncSender<Payload>>>> = self
           .writers
-          .get(target)
+          .get(&target)
           .map(|WriterContext { live_outputs, .. }| live_outputs.clone())
           .expect("writer stream id should already be registered");
-        if let Some(_) = live_outputs.write().insert(new_read_id, sender.clone()) {
-          unreachable!(
-            "this read target is new, so should not have been registered to any write targets yet"
-          );
-        }
+        assert!(live_outputs
+          .write()
+          .insert(new_read_id, sender.clone())
+          .is_none());
       }
       new_read_id
     }
@@ -342,16 +367,16 @@ pub mod mux {
       }
     }
 
-    fn find_matching_listeners(
-      &self,
-      name: &Name,
-    ) -> impl Iterator<Item=(ReadStreamId, mpsc::SyncSender<Payload>)> {
+    fn find_matching_listeners<'a>(
+      &'a self,
+      name: &'a Name,
+    ) -> impl Iterator<Item=(ReadStreamId, mpsc::SyncSender<Payload>)>+'a {
       self
         .listeners
         .iter()
         .filter_map(|(listener_id, ListenerContext { query, sender, .. })| {
           if query.matches(name) {
-            Some((listener_id, sender.clone()))
+            Some((*listener_id, sender.clone()))
           } else {
             None
           }
@@ -362,17 +387,15 @@ pub mod mux {
       let new_write_id = WriteStreamId::new();
       let current_listeners: IndexMap<ReadStreamId, mpsc::SyncSender<Payload>> =
         self.find_matching_listeners(&name).collect();
-      let listener_ids: Vec<_> = current_listeners.keys().collect();
+      let listener_ids: Vec<_> = current_listeners.keys().cloned().collect();
 
-      if let Some(_) = self.writers.insert(new_write_id, WriterContext {
-        name,
-        live_outputs: Arc::new(RwLock::new(current_listeners)),
-      }) {
-        unreachable!(
-          "assumed writer stream id {:?} was not already registered",
-          new_write_id
-        );
-      }
+      assert!(self
+        .writers
+        .insert(new_write_id, WriterContext {
+          name,
+          live_outputs: Arc::new(RwLock::new(current_listeners)),
+        })
+        .is_none());
 
       for target in listener_ids.into_iter() {
         let live_inputs: Arc<RwLock<IndexSet<WriteStreamId>>> = self
@@ -396,7 +419,7 @@ pub mod mux {
       let final_outputs: IndexMap<ReadStreamId, _> = Arc::into_inner(live_outputs)
         .expect("should be the only handle to live_outputs data")
         .into_inner();
-      for targets in final_outputs.into_keys() {
+      for target in final_outputs.into_keys() {
         let live_inputs: Arc<RwLock<IndexSet<WriteStreamId>>> = self
           .listeners
           .get(&target)
@@ -433,6 +456,7 @@ pub mod mux {
     }
   }
 
+  /* TODO: how to set this? */
   const CHANNEL_SIZE: usize = 50;
 
   impl Streamer for MuxHandle {
@@ -441,16 +465,81 @@ pub mod mux {
     type Query = Query;
     type Writer = WriteMux;
 
-    fn open_listener(&mut self, query: Self::Query) -> Self::Listener {
+    fn open_listener(&self, query: Self::Query) -> Self::Listener {
       let (sender, receiver) = mpsc::sync_channel::<Payload>(CHANNEL_SIZE);
       let id = self.mux.write().add_new_listener(query, sender);
       ListenMux::new(id, receiver, self.mux.clone())
     }
 
-    fn open_writer(&mut self, name: Self::Name) -> Self::Writer {
+    fn open_writer(&self, name: Self::Name) -> Self::Writer {
       let id = self.mux.write().add_new_writer(name);
       let live_outputs = self.mux.read().live_outputs_for_writer(&id);
       WriteMux::new(id, live_outputs, self.mux.clone())
     }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::{
+    indexing,
+    mux::{self, ListenStream, Streamer},
+  };
+
+  use std::io::{Read, Write};
+
+  #[test]
+  fn interleaved() {
+    let mux_handle = mux::MuxHandle::new();
+
+    let mut listener1 = mux_handle.open_listener(indexing::Query("asdf".to_string()));
+    let mut other_listener = mux_handle.open_listener(indexing::Query("bbb".to_string()));
+
+    let mut listener2 = {
+      let mut writer1 = mux_handle.open_writer(indexing::Name("asdf".to_string()));
+      writer1.write_all(b"wow").unwrap();
+
+      /* This should not interact with the "asdf" streams at all. */
+      let mut other_writer = mux_handle.open_writer(indexing::Name("bbb".to_string()));
+      other_writer.write_all(b"isolated").unwrap();
+
+      let listener2 = mux_handle.open_listener(indexing::Query("asdf".to_string()));
+      {
+        let mut writer2 = mux_handle.open_writer(indexing::Name("asdf".to_string()));
+        writer2.write_all(b"hey").unwrap();
+      }
+
+      writer1.write_all(b"woah").unwrap();
+
+      listener2
+    };
+
+    let mut buf1 = [0u8; 10];
+    listener1.read_exact(&mut buf1).unwrap();
+    assert_eq!(&buf1, b"wowheywoah");
+    let mut buf2 = [0u8; 7];
+    listener2.read_exact(&mut buf2).unwrap();
+    assert_eq!(&buf2, b"heywoah");
+
+    let mut other_buf = [0u8; 8];
+    other_listener.read_exact(&mut other_buf).unwrap();
+    assert_eq!(&other_buf, b"isolated");
+  }
+
+  #[test]
+  fn signal_end() {
+    let mux_handle = mux::MuxHandle::new();
+
+    let mut listener = mux_handle.open_listener(indexing::Query("asdf".to_string()));
+
+    let mut writer = mux_handle.open_writer(indexing::Name("asdf".to_string()));
+    writer.write_all(b"wow").unwrap();
+
+    /* Without this, the call to .read_to_end() would never return. */
+    listener.signal_end();
+
+    let mut buf: Vec<u8> = Vec::new();
+    listener.read_to_end(&mut buf).unwrap();
+    assert_eq!(&buf, b"wow");
   }
 }
